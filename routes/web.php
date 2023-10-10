feca03fb6baa851e765255e4521ec6679f6d8833
Zé Marcos <contato.marcosrf@gmail.com>
2023-10-10 13:08:23 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return redirect()->route('products.index');
+});
+
 Route::get('products', [ProductController::class, 'index'])->name('products.index');
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
 Route::post('products', [ProductController::class, 'store'])->name('products.store');
