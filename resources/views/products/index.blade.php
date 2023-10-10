520c94f975258298dd1b64bee3b9b24377ad5f23
Zé Marcos <contato.marcosrf@gmail.com>
2023-10-09 23:29:22 -0300

    create page for list all products

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,60 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Catálago de Produtos
+        </h2>
+    </x-slot>
+    <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="m-5 overflow-hidden border border-gray-200 rounded-lg shadow-md">
+            <table class="w-full text-sm text-left text-gray-500 bg-white border-collapse">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">ID</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Description</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Price</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Quantity</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900">Updated At</th>
+                        <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
+                    </tr>
+                </thead>
+                <tbody class="border-t border-gray-100 divide-y divide-gray-100">
+                    @foreach ($products as $product)
+                        <tr class="hover:bg-gray-50">
+                            <th class="flex gap-3 px-6">{{ $product->id }}</th>
+                            <td class="px-6 py-4">{{ $product->name }}</td>
+                            <td class="px-6 py-4">{{ Str::of($product->description)->limit(20) }}</td>
+                            <td class="px-6 py-4">R$ {{ $product->price }}</td>
+                            <td class="px-6 py-4">{{ $product->quantity }}</td>
+                            <td class="px-6 py-4">{{ $product->updated_at->format('d/m/Y H:i:s') }}</td>
+                            <td class="px-6 py-4">
+                            <td class="px-6 py-4">
+                                <div class="flex justify-end gap-4">
+                                    <a x-data="{ tooltip: 'Delete' }" href="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6"
+                                            x-tooltip="tooltip">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                        </svg>
+                                    </a>
+                                    <a x-data="{ tooltip: 'Edite' }" href="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6"
+                                            x-tooltip="tooltip">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </td>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</x-app-layout>
